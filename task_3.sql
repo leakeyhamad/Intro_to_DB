d1d2587b73d9362d4875dabf45f8d30faddf7ad4
Leakey <otienoleakey287@gmail.com>
2025-12-14 08:44:28 +0300

    Made changes in task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,2 +1,5 @@
--- List all tables in the database provided via command line argument
+-- Select the database provided in the command line context
+USE alx_book_store;
+
+-- List all tables in the specified database
 SHOW TABLES;
